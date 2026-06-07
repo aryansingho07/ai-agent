ba4f7a0adc4ff9c5810f3b5ec1490c107e8a41fb
aryansingho07 <aryansingh99840@gmail.com>
2026-06-08 00:27:23 +0530

    change in agent

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+import "dotenv/config";
 import { GoogleGenAI } from "@google/genai";
 
-const GEMINI_API_KEY = "AIzaSyDTvYIoZOhI7nV0OwtjgKHU_fgug2f7e80";
-
 const ai = new GoogleGenAI({
-  apiKey: GEMINI_API_KEY,
+  apiKey: process.env.GEMINI_API_KEY,
 });
+//Tools
+function getWeatherDetails(city) {
+  if(city.toLowerCase() === "patiala") {
+    return "40°C";
+  }
+  if(city.toLowerCase() === "mohali") {
+    return "30°C";
+  }
+  if(city.toLowerCase() === "kashipur") {
+  return "42°C" }
+}
+// This is a system prompt that defines the tools available to the agent and provides an example of how to use them. The agent will use this information to plan its actions and generate responses.
+const SYSTEM_PROMPT = `
+Available Tools:
 
+- function getWeatherDetails(city: string): string
+
+getWeatherDetails is a function that accepts city name
+and returns weather details
+
+Example:
+START
+{ "type": "user", "user": "What is the sum of weather of Patiala and Mohali?" }
+{ "type": "plan", "plan": "I will call getWeatherDetails for Patiala" }
+{ "type": "action", "function": "getWeatherDetails", "input": "patiala" }
+{ "type": "observation", "observation": "10°C" }
+{ "type": "plan", "plan": "I will call getWeatherDetails for Mohali" }
+{ "type": "action", "function": "getWeatherDetails", "input": "mohali" }
+{ "type": "observation", "observation": "14°C" }
+{ "type": "output", "output": "The sum is 24°C" }
+`;
 const response = await ai.models.generateContent({
   model: "gemini-2.5-flash",
-  contents: "wheater of kashipur,uttarakhand",
+  contents:'{ "type": "observation", "observation": "25°C" }',
+  config: {
+    systemInstruction: SYSTEM_PROMPT,
+  }
 });
 
 console.log(response.text);
